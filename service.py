27f898e39b5cd28d339aefa2fbe4e4f4eaf9e9fe
GregorZamza94 <0580941@gmail.com>
2022-10-27 01:56:58 +0300

    Add files via upload

---
+++ service.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+import bentoml
+from bentoml.io import NumpyNdarray
+
+model_ref = bentoml.sklearn.get("mlzoomcamp_homework:qtzdz3slg6mwwdu5")
+
+model_runner = model_ref.to_runner()
+
+svc = bentoml.Service("mlzoomcamp_homework", runners=[model_runner])
+
+@svc.api(input=NumpyNdarray(shape=(-1,4), enforce_shape=True), output=NumpyNdarray())
+async def classify(vector):
+    prediction = model_runner.predict.run(vector)
+    
+    return prediction
